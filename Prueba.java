b7b9bf509028d9acf910f46505f85e10d7874ec3
Alumno <Alumno@A203_PC09>
2019-10-03 13:18:48 +0200

    He puesto mi año de  nacimiento

--- Prueba.java
+++ Prueba.java
@@ -3,6 +3,8 @@ public class Prueba {
 	public static void main(String args[]) {
 
 		System.out.println("Diego Fernández Monge");
+		System.out.println("León");
+		System.out.println("1999");
 
 	}
 
